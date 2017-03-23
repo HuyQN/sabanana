743981520758e0495fe78caf9a3660644cf2ecab
Saul Shanabrook <s.shanabrook@gmail.com>
2017-03-23 17:06:17 -0400

    turn User into components

--- app/app.js
+++ app/app.js
@@ -1,20 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react'
+import ReactDOM from 'react-dom'
 
 // Each major browser view user interface must be imported.
-import UI01 from './components/ui-01.js';
-import UI02 from './components/ui-02.js';
+import User from './components/user.js'
+import UI02 from './components/ui-02.js'
 
 // For each view conditionally determine which view to display
 // depending on if the ID is present in the HTML.
-if (document.getElementById('ui-01') !== null) {
+if (document.getElementById('user') !== null) {
   ReactDOM.render(
-    <UI01 />,
-    document.getElementById('ui-01')
-  );
+    <User />,
+    document.getElementById('user')
+  )
 } else if (document.getElementById('ui-02') !== null) {
   ReactDOM.render(
     <UI02 />,
     document.getElementById('ui-02')
-  );
+  )
 }
