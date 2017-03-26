8779b61b9b7d6c87e2b2b6c3efa4ef901293cf49
Evan Busha <ebusha@umass.edu>
2017-03-26 16:15:17 -0400

    Fixed compilation errors and removed ui-01 and ui-02 related code.

--- app/app.js
+++ app/app.js
@@ -2,8 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 // Each major browser view user interface must be imported.
-import UI01 from './components/ui-01.js';
-import UI02 from './components/ui-02.js';
 import AddPost from './components/addpost.js'
 import EditPost from './components/editpost.js'
 import User from './components/User.js'
@@ -15,11 +13,6 @@ if (document.getElementById('user') !== null) {
   ReactDOM.render(
     <User />,
     document.getElementById('user')
-  );
-} else if (document.getElementById('ui-02') !== null) {
-  ReactDOM.render(
-    <UI02 />,
-    document.getElementById('ui-02')
   );
 } else if (document.getElementById('add-post') !== null) {
   ReactDOM.render(
