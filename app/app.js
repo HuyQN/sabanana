b01f1c21f12c4a8542b14ea5cd60cff555f8db30
Avery Samuel <aasamuel@umass.edu>
2017-03-26 14:17:42 -0400

    renamed display_screen to displaypost and added displaypost.js

--- app/app.js
+++ app/app.js
@@ -2,8 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 // Each major browser view user interface must be imported.
-import UI01 from './components/ui-01.js';
-import UI02 from './components/ui-02.js';
+import Displaypost from './components/displaypost.js'
 import AddPost from './components/addpost.js'
 import EditPost from './components/editpost.js'
 import User from './components/User.js'
@@ -16,12 +15,7 @@ if (document.getElementById('user') !== null) {
     <User />,
     document.getElementById('user')
   );
-} else if (document.getElementById('ui-02') !== null) {
-  ReactDOM.render(
-    <UI02 />,
-    document.getElementById('ui-02')
-  );
-} else if (document.getElementById('add-post') !== null) {
+}  else if (document.getElementById('add-post') !== null) {
   ReactDOM.render(
     <AddPost />,
     document.getElementById('add-post')
@@ -30,6 +24,12 @@ if (document.getElementById('user') !== null) {
   ReactDOM.render(
     <EditPost title="Current Title" description="Current Description"></EditPost>,
     document.getElementById('edit-post')
+  );
+}
+else if (document.getElementById('displaypost') !== null) {
+  ReactDOM.render(
+    <Displaypost />,
+    document.getElementById('displaypost')
   );
 }
 
