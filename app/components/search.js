e8a45098921122a65eba93d92fa33edb6ccb4f1d
Evan Busha <ebusha@umass.edu>
2017-04-10 14:42:57 -0400

    Allowed for selecting and deselecting tags

--- app/components/search.js
+++ app/components/search.js
@@ -20,17 +20,53 @@ var posts = [{
 }]
 
 class Tagbar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      tags: this.props.tags,
+      selected_tags: this.props.selected_tags
+    }
+  }
+
+  diff(a, b) {
+    return a.filter((i) => {return b.indexOf(i) < 0;});
+  }
+
+  handleSelect(tag) {
+    var index = this.state.tags.indexOf(tag);
+    this.state.tags.splice(index,1);
+    this.state.selected_tags.push(tag);
+    this.setState({});
+  }
+
+  handleDeselect(tag) {
+    var index = this.state.selected_tags.indexOf(tag);
+    this.state.selected_tags.splice(index,1);
+    this.state.tags.push(tag);
+    this.setState({});
+  }
+
   render() {
+    var disjoint = this.diff(this.state.tags, this.state.selected_tags);
+    
     return (
       <div className="col-md-2 panel panel-default tags">
         <ul className="nav">
           <li role="presentation">TAGS</li>
-          {this.props.tags.map((tag) => {
+          {this.state.selected_tags.map((tag) => {
              return(
                <li role="presentation">
-                 <a href="#">
+                 <label type="button" className="btn btn-default active" type="button" onClick={() => this.handleDeselect(tag)}>
                    {tag}
-                 </a>
+                 </label>
+               </li>
+             )})}
+          {disjoint.map((tag) => {
+             return(
+               <li role="presentation">
+                 <label type="button" className="btn btn-default" type="button" onClick={() => this.handleSelect(tag)}>
+                   {tag}
+                 </label>
                </li>
              )})}
         </ul>
@@ -58,7 +94,10 @@ export default class Search extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      tags: ["Board Games", "Sports", "Music", "Computers", "Clothing", "Language"]
+      /* Tags will need to be the result of a call to the server*/
+      tags: ["Board Games", "Sports", "Music", "Computers", "Clothing", "Language"],
+      /* Always start with no selected tags*/
+      selected_tags: ["Test"]
     }
   }
 
@@ -66,7 +105,7 @@ export default class Search extends React.Component {
     return (
       <div className="container-fluid">
         <div className="clearfix">
-          <Tagbar tags={this.state.tags} />
+          <Tagbar tags={this.state.tags} selected_tags={this.state.selected_tags} />
           <SearchResults search_term={this.props.match.params.query}>
             <Posts posts={posts} />
           </SearchResults>
