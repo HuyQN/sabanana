a2976680552fdc12dfa67f3096e0f8c7bfdf5a79
Saul Shanabrook <s.shanabrook@gmail.com>
2017-04-23 14:17:44 -0400

    add message link  to user profile

--- app/components/User.js
+++ app/components/User.js
@@ -4,6 +4,7 @@ import Posts from './Posts'
 //import {user} from '../propTypes'
 import getUsersPosts from '../server.js'
 import Bio from './Bio'
+import MessageLink from './MessageLink'
 
 export default class User extends React.Component {
   constructor (props) {
@@ -28,6 +29,13 @@ export default class User extends React.Component {
       <div className='container'>
         <div className='row'>
           <Bio user={this.state.user} />
+        </div>
+        <div className='row'>
+          <div className='col-md-4'>
+            <div className='btn-group' role='group' aria-label='...'>
+              <MessageLink user={this.state.user} />
+            </div>
+          </div>
         </div>
       </div>
     )
