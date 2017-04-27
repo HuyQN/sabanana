21f603cb671bb7b060e8e0cd96b462b73957f927
Huy Nguyen <huyqnguyen@umass.edu>
2017-04-26 23:05:30 -0400

    added post schemas, able to create post on server

--- server/src/server.js
+++ server/src/server.js
@@ -1,7 +1,12 @@
 var express = require('express')
 var bodyParser = require('body-parser')
-
-var views = require('./views')
+var database = require('./database')
+var readDocument = database.readDocument;
+var writeDocument = database.writeDocument;
+var addDocument = database.addDocument;
+var postSchema = require('./schemas/post.json');
+var validate = require('express-jsonschema').validate;
+var views = require('./views');
 
 var app = express()
 
@@ -17,8 +22,28 @@ var allowCrossDomain = function (req, res, next) {
 app.use(bodyParser.json())
 app.use(allowCrossDomain)
 
+function createPost(author, name, description, tags){
+  var date = new Date().getTime();
+  var newPost = {
+    "authorID": author,
+    "name": name,
+    "description": description,
+    "tags": tags,
+    "date": date
+  };
+  newPost = addDocument('post', newPost);
+  return newPost;
+}
+
 app.get('/posts/', function (req, res) {
   res.send(views.getAllPosts())
+})
+
+app.post('/post/', validate({body: postSchema}) , function(req,res){
+  var body= req.body;
+  var newPost = createPost(body.authorID, body.name, body.description, body.tags);
+  res.status(201);
+  res.send(newPost);
 })
 
 app.listen(3000, function () {
