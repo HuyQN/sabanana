1e2ed0ae4e1ccbfc3f0880a3b165e44b7cadecd2
Kazi S Ahmed <Kazi255@gmail.com>
2017-04-27 15:36:20 -0400

    Noticed more lines missing semicolons

--- server/src/server.js
+++ server/src/server.js
@@ -12,11 +12,10 @@ var app = express();
 
 // http://stackoverflow.com/a/7069902/907060
 var allowCrossDomain = function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*')
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
-  res.header('Access-Control-Allow-Headers', 'Content-Type')
-
-  next()
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
 };
 
 app.use(bodyParser.json());
